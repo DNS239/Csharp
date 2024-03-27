45569124b496f41b6c2df7c681221b6995ebb568
Dns <102502216+DNS239@users.noreply.github.com>
2024-03-26 22:45:11 -0300

    #

--- Propriedades/obj/Debug/net8.0/Propriedades.AssemblyInfo.cs
+++ Propriedades/obj/Debug/net8.0/Propriedades.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ExemplosExplorando")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Propriedades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ExemplosExplorando")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ExemplosExplorando")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9acf0e6d23cad68db318f0337225208211401291")]
+[assembly: System.Reflection.AssemblyProductAttribute("Propriedades")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Propriedades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
